d16e1b696f0cb21cffab571ba23a38cb28468e82
Deban Kumar Sahu <sahudebankumar@gmail.com>
2025-12-28 11:47:11 +0530

    butFix(entry point): Added missing middlewares like CORS and SESSION. Fixed the router connectivity with main entry point.

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
+from app.api.v1.route.auth import authRouter as authRouterV1
 from app.config import settings
 
 app = FastAPI(title="CraftLet")
@@ -8,3 +10,13 @@ app = FastAPI(title="CraftLet")
 app.add_middleware(
     SessionMiddleware, secret_key=settings.appSettings.SESSION_SECRET_KEY
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(authRouterV1)
